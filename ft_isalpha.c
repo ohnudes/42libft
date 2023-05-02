7a72b2ae60e484575d0f0c5f1676b0b7168dbe22
Nudes <nivohq@gmail.com>
2023-05-02 11:16:05 +0200

    return based on boolean

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -5,7 +5,11 @@
  *	ft_isalpha argument must be representable as an unsigned char or the value
  *	of EOF
  */
-
+int	ft_isalpha(int c)
+{
+	return((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+}
+/*
 int	ft_isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
@@ -14,7 +18,7 @@ int	ft_isalpha(int c)
 		return(c);
 	else
 		return(0);
-}
+}*/
 
 /* To check:
  *		Return value for true.
