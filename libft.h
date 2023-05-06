70925caf4e8f000f4a3e2e7da6312a1d64b98a49
Nudes <nivohq@gmail.com>
2023-05-06 23:18:09 +0200

    Edit: ft_bzero declaration was wrong: (void *) to (void)

--- libft.h
+++ libft.h
@@ -20,7 +20,7 @@ char	*ft_strrchr(const char *str, int c);
 size_t	ft_strlen(const char *c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-void	*ft_bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t);
