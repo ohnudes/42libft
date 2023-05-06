5980c0935192ab2fd174d473370a3adbfa5faec4
Nudes <nivohq@gmail.com>
2023-05-06 22:36:45 +0200

    Format: optimized without counters nor index nor shits. We're free of them now!

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,19 +1,18 @@
+/* Ascii to Int */
+
 #include "libft.h"
 
 int	ft_atoi(const char *nptr)
 {
-	size_t	index;
 	char *cast;
 	int	result;
 
 	cast = (char*)nptr;
-	index = 0;
 	result = 0;
-	while (ft_isdigit(cast[index]))
+	while (*nptr && ft_isdigit(*nptr))
 	{
-		result = result * 10 + (cast[index] - 48);
-		index++;
+		result = result * 10 + (*nptr - 48);
+		nptr++;
 	}
 	return(result);
 }
-
