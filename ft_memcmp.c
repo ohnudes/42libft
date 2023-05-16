5ce79360fd166eacd1aebd49358469d64d06ad48
Nicolas Maturana Chandia <nmaturan@car8s4.42barcelona.com>
2023-05-16 18:26:20 +0200

    Update: ft_memcmp fixed.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -16,16 +16,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*c_s1;
 	unsigned char	*c_s2;
+	size_t			i;
 
 	c_s1 = (unsigned char *)s1;
 	c_s2 = (unsigned char *)s2;
+	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1 == s2 && n)
-	{
-		c_s1++;
-		c_s2++;
-		n--;
-	}
-	return (s1 - s2);
+	while (c_s1[i] == c_s2[i] && i < n - 1)
+		i++;
+	return ((int)(c_s1[i]-c_s2[i]));
 }
