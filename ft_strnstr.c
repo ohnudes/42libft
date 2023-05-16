d188443860bc767cd8b7ab79bdbf727d1e6b4300
Nudes <nivohq@gmail.com>
2023-05-16 22:06:25 +0200

    Format: ft_strnstr further cleaning.

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:34:56 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/05/16 22:04:30 by nmaturan         ###   ########.fr       */
+/*   Updated: 2023/05/16 22:06:15 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,43 +37,3 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (NULL);
 }
-/*
-   char	*ft_strnstr(const char *big, const char *little, size_t len)
-   {
-   size_t	i;
-   size_t	j;
-
-   i = 0;
-   j = 0;
-   if (!*little)
-   return ((char *)big);
-   if (len < ft_strlen(little))
-   return (NULL);
-   while (big[i])
-   {
-   printf("i = %zu\n", i);
-   if (big[i + j] == little[j])
-   {
-   while (big[i + j] == little[j] && j < ft_strlen(little))
-   {
-   printf("j = %zu\n", j);
-   if (little[j] == 0)
-   return ((char *)big + i);
-   j++;
-   }
-   if (little[j] == 0)
-   return ((char *)big + i);
-   j = 0;
-   }
-   i++;
-   }
-   return (NULL);
-   }
-
-   int	main(void)
-   {
-   const char	*big = "12312345678";
-   const char	*little = "2";
-   printf("result: %s\n", ft_strnstr(big, little, 20));
-   return (0);
-   }*/
