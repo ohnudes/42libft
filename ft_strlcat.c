cf71dd75b8c8f7d638e7db6d5905b4928d3d2e7d
Nudes <nivohq@gmail.com>
2023-05-17 10:51:12 +0200

    Update: ft_strlcat without noise.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,25 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/15 17:51:33 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/05/15 17:51:40 by nmaturan         ###   ########.fr       */
+/*   Created: 2023/05/17 10:50:54 by nmaturan          #+#    #+#             */
+/*   Updated: 2023/05/17 10:51:01 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nmaturan <nmaturan@student.42barcelona.co  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/07 15:17:42 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/05/11 10:28:01 by nmaturan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
-//#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -46,13 +34,3 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst[dstsize - 1] = '\0';
 	return (len_src + len_dst);
 }
-/*int	main(int argc, char **argv)
-{
-	char	dst[10] = "012345678";
-
-	if (argc != 2)
-		return (0);
-	printf("%zu\n", ft_strlcat(dst, argv[1], ft_strlen(dst)));
-	printf("%s\n", dst);
-	return (0);
-}*/
