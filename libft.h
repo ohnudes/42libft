1d58c216867b271bf8bf6c81cb89ae47748a30bf
Nicolas Maturana Chandia <nmaturan@car9s6.42barcelona.com>
2023-05-17 16:47:25 +0200

    Update: conflicting types fixed for ft_strdup

--- libft.h
+++ libft.h
@@ -37,8 +37,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_calloc(size_t memb, size_t size);
-char	ft_strdup(const char *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
