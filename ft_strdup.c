27a514b05d3609a7314681d357f754476608a3d2
Nicolas Maturana Chandia <nmaturan@car9s6.42barcelona.com>
2023-05-17 17:41:41 +0200

    Update: ft_strdup now ready.

--- ft_strdup.c
+++ ft_strdup.c
@@ -30,37 +30,3 @@ char	*ft_strdup(const char *s)
 	}
 	return (s_cpy);
 }
-/*
-#include "libft.h"
-#include <stdio.h>
-
-char	*ft_strdup(const char *s)
-{
-	char	*s_cpy;
-	size_t	s_len;
-	size_t	i;
-
-	s_len = ft_strlen(s);
-	s_cpy = malloc(sizeof(char) * s_len);
-	i = 0;
-	if (!s_cpy)
-		return (NULL);
-	while (i <= s_len)
-	{
-		s_cpy[i++] = s[i++];
-		printf("cpy[%zu]=%c\n",i,s_cpy[i]);
-		i++;
-	}
-	return (s_cpy);
-}
-
-int	main(void)
-{
-	char	*s = "123";
-	char	*s_cpy;
-
-	s_cpy = ft_strdup(s);
-	printf("s = %s\ns_cpy = %s\n", s, s_cpy);
-	free(s_cpy);
-	return (0);
-}*/
