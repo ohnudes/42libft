b3cee4b3f9327488985b534f5671a2d082cd81e8
Nudes <nivohq@gmail.com>
2023-05-19 11:51:29 +0200

    Update: calloc modified, strlen for count, given that count is already the index.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:35:29 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/05/16 14:35:31 by nmaturan         ###   ########.fr       */
+/*   Updated: 2023/05/18 17:38:17 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	*ft_calloc(size_t count, size_t size)
 	ptr = malloc(size * count);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, ft_strlen(ptr));
+	ft_bzero(ptr, count);
 	return (ptr);
 }
