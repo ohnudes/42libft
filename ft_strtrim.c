b487bb8a37a89c68894c5ef8bd7da85008344183
Nudes <nivohq@gmail.com>
2023-05-21 17:20:18 +0200

    Update: updated trim: only real change is to set ri to strlen(s1) and only keep the ri-1 when checking the starting trim.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,8 +1,21 @@
-#include "libft.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/21 15:52:54 by nmaturan          #+#    #+#             */
+/*   Updated: 2023/05/21 17:20:14 by nmaturan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ft_isset(const char *set, char c)
+#include "libft.h"
+//#include <stdio.h>
+
+static	int	isset(const char *set, char c)
 {
+
 	while (*set)
 	{
 		if (*set == c)
@@ -18,38 +31,62 @@ char	*ft_strtrim(const char *s1, const char *set)
 	size_t	ri;
 	char	*res;
 
-	i = 0;
-	ri = ft_strlen(s1) - 1;
-	printf("check 0:\n i = %zu\nri = %zu\n", i, ri);
-
-	while (s1[i] && i < ri && (ft_isset(set, s1[i]) == 1))
-		i++;
-	printf("check 1: i = %zu\n", i);
-
-	while (ft_isset(set, s1[ri]) == 1)
-		ri--;
-	printf("check 2: ri = %zu\n", ri);
-
-	if (s1[i] == s1[ri])
+	if (!s1)
 		return (ft_strdup(""));
-	printf("check 3 valid string\n");
-
+	i = 0;
+	ri = ft_strlen(s1);
+	while (i < ri && isset(set, s1[i]))
+		i++;
+	while (ri > i && isset(set, s1[ri - 1]))
+		ri--;
+	if (i == ri)
+		return (ft_strdup(""));
 	res = malloc(sizeof(char) * (ri - i + 1));
 	if (!res)
 		return (NULL);
-	printf("check 4: res created\n");
-
-	res = ft_substr(s1, i, (ri - i + 1));
-	printf("check 5: res = %s\n", res);
+	ft_strlcpy(res,s1 + i, ri - i + 1);
 	return (res);
 }
 
-int	main(void)
-{
-	char	*res;
+/*
+   char	*ft_strtrim(const char *s1, const char *set)
+   {
+   size_t	i;
+   size_t	ri;
+   char	*res;
 
-	res = ft_strtrim("21111ho1la1112", "12");
-	printf("result = %s\n", res);
-	free (res);
-	return (0);
-}
+   i = 0;
+   ri = ft_strlen(s1) - 1;
+   printf("check 0:\n i = %zu\nri = %zu\n", i, ri);
+
+   while (s1[i] && i < ri && (isset(set, s1[i])))
+   i++;
+   printf("check 1: i = %zu\n", i);
+
+   while (isset(set, s1[ri]))
+   ri--;
+   printf("check 2: ri = %zu\n s1[ri] = %s\n", ri, s1 + ri);
+
+   if (i == ri)
+   return (ft_strdup(""));
+   printf("check 3 valid string\n");
+
+   res = malloc(sizeof(char) * (ri - i + 1));
+   if (!res)
+   return (NULL);
+   printf("check 4: res created with maxlen = %zu\n", ri-i+1);
+
+   res = ft_substr(s1, i, ri - i + 1);
+   printf("check 5: res = %s\n", res);
+   return (res);
+   }
+
+   int	main(void)
+   {
+   char	*res;
+
+   res = ft_strtrim("x zxz xz 21111ho1la1112 xxzxz z x", "z x");
+   printf("result = %s\nres_len = %zu\n", res, ft_strlen(res));
+   free (res);
+   return (0);
+   }*/
