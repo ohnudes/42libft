8d723e7ec6f64e47e3711aff87a6a7b6b76e709c
Nudes <nivohq@gmail.com>
2023-05-21 15:25:28 +0200

    Update: function was lacking a second counter.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -4,8 +4,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*res;
 	size_t	i;
+	size_t	j;
 
 	i = 0;
+	j = 0;
 	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!res)
 		return (NULL);
@@ -14,11 +16,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		res[i] = s1[i];
 		i++;
 	}
-	while (i < ft_strlen(s2))
+	while (j < ft_strlen(s2))
 	{
-		res[i] = s2[i];
-		i++;
+		res[i + j] = s2[j];
+		j++;
 	}
-	res[i] = '\0';
+	res[i + j] = '\0';
 	return (res);
 }
