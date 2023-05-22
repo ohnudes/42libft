240effd7acedb430481f39fb2b7d4d07a6ba9879
Nicolas Maturana Chandia <nmaturan@car6s4.42barcelona.com>
2023-05-22 18:17:03 +0200

    Update: ft_split is alive! freeing some  not so allocated memory.

--- ft_split.c
+++ ft_split.c
@@ -5,15 +5,16 @@ static void free_all(char	**big, size_t count_check)
 {
 	if (big)
 	{
-		if (!big[count_check])
+		if (count_check)
 		{
 			while (count_check <= 0)
 			{
 				free(big + count_check);
 				count_check--;
 			}
-			free(big);
 		}
+		printf("Step 2: big created\n");
+		free(big);
 	}
 	if (!big)
 		free(big);
@@ -35,9 +36,9 @@ static size_t str_count(const char *s, const char c)
 		while (s[i] && s[i] == c) 
 			i++;
 	}
+	printf("Step 1: how many substrings = %zu\n", count);
 	return (count);
 }
-
 static size_t str_len(const char *s, const char c) 
 {
 	size_t len;
@@ -45,25 +46,29 @@ static size_t str_len(const char *s, const char c)
 	len = 0;
 	while (s[len] && s[len] != c)
 		len++;
+	printf("str_len = %zu\n", len);
 	return (len);
 }
-
-char **ft_split(const char *s, const char c) 
+/*
+*/
+void ft_split(const char *s, const char c) 
 {
 	char	**big;
 	size_t	little;
 	size_t	i;
 
 	little = 0;
-	big = (char **)malloc(sizeof(char *) * (str_count(s, c) + 1));
+	printf("Step 0: pre malloc\n");
+	big = (char **)malloc(sizeof(char **) * (str_count(s, c) + 1));
 	free_all(big, little);
+	printf("Step 3: post free\n");
 	i = 0;
 	while (little < str_count(s, c)) 
 	{
 		if (s[i] && s[i] != c)
 		{
 			big[little] = ft_substr(s, i, str_len((s + i), c) + 1); //free!
-			free_all (big, little);
+			//free_all(big, little);
 			little++;
 		}
 		while (s[i] && s[i] != c)
@@ -71,16 +76,17 @@ char **ft_split(const char *s, const char c)
 		while (s[i] && s[i] == c)
 			i++;
 	}
+	free_all(big, little);
+	/*
 	return (big);
+	*/
 }
 
 int	main(void)
 {
-	char *test;
-	char *a = "123 123 1";
+	char *a = "1 22 333";
 	const char c = ' ';
 	
-	test = ft_split(a, c);
-	printf("test = %s\n", test);
+	ft_split(a, c);
 	return (0);
 }
