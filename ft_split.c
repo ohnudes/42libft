1ac7158379bc383642f15bd655b0543305038ead
Nudes <nivohq@gmail.com>
2023-05-23 13:34:08 +0200

    Fix: split completed.

--- ft_split.c
+++ ft_split.c
@@ -6,29 +6,21 @@
 /*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 09:59:50 by nmaturan          #+#    #+#             */
-/*   Updated: 2023/05/23 12:29:44 by nmaturan         ###   ########.fr       */
+/*   Updated: 2023/05/23 13:34:05 by nmaturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	free_all(char **big, size_t little)
+static char	**free_all(char **big, size_t little)
 {
-	size_t	i;
-
-	i = 0;
-	if (big[little] == NULL)
+	while (little)
 	{
-		free(big[i]);
-		while (i < little)
-		{
-			i++;
-			free(big[i]);
-		}
-		free(big);
-		return (1);
+		little--;
+		free(big[little]);
 	}
-	return (0);
+	free(big);
+	return (NULL);
 }
 
 static size_t	str_count(const char *s, const char c)
@@ -66,18 +58,18 @@ char	**ft_split(const char *s, const char c)
 	size_t	little;
 	size_t	i;
 
-	if (!s)
-		return (NULL);
 	big = malloc(sizeof(char **) * (str_count(s, c) + 1));
+	if (!big)
+		return (NULL);
 	little = 0;
 	i = 0;
-	while (s[i])
+	while (little < str_count(s, c))
 	{
-		if (s[i] != c)
+		if (s[i] && s[i] != c)
 		{
 			big[little] = ft_substr(s, i, str_len((s + i), c));
-			if (free_all(big, little))
-				return (NULL);
+			if (!big[little])
+				return (free_all(big, little));
 			little++;
 		}
 		while (s[i] && s[i] != c)
@@ -85,7 +77,6 @@ char	**ft_split(const char *s, const char c)
 		while (s[i] && s[i] == c)
 			i++;
 	}
-	little++;
 	big[little] = NULL;
 	return (big);
 }
@@ -94,16 +85,16 @@ int	main(void)
 {
 	//char *a = "";
 	//const char c = '\0';
-	char *a = "hello !   2 3     5";
+	char *a = "      split       this for   me  !       ";
 	const char c = ' ';
 	size_t	i = 0;
 
-	char **result = ft_split(a, 32);
+	char **result = ft_split(a, c);
 	if (!result)
 		free(result);
 	else
 	{
-		while (i <= str_count(a, 32))
+		while (i <= str_count(a, c))
 		{
 			printf("substring %zu == %s\n", i, result[i]);
 			i++;
