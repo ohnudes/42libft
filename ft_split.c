a0b8734c838d7bbf552476cc63258681ac3a15e8
Nudes <nivohq@gmail.com>
2023-05-23 12:29:52 +0200

    Update: functional split, errors to be checked.

--- ft_split.c
+++ ft_split.c
@@ -1,76 +1,83 @@
-#include "libft.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaturan <nmaturan@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 09:59:50 by nmaturan          #+#    #+#             */
+/*   Updated: 2023/05/23 12:29:44 by nmaturan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static void free_all(char	**big, size_t count_check)
+#include "libft.h"
+
+static int	free_all(char **big, size_t little)
 {
-	if (big)
+	size_t	i;
+
+	i = 0;
+	if (big[little] == NULL)
 	{
-		if (count_check)
+		free(big[i]);
+		while (i < little)
 		{
-			free(big + count_check);
-			while (count_check != 0)
-			{
-				count_check--;
-				free(big + count_check);
-				printf("big + %zu freed\n", count_check);
-			}
+			i++;
+			free(big[i]);
 		}
-		printf("Step 2: big created\n");
-		//free(big);
-	}
-	if (!big)
 		free(big);
+		return (1);
+	}
+	return (0);
 }
 
-static size_t str_count(const char *s, const char c)
+static size_t	str_count(const char *s, const char c)
 {
-	size_t count;
-	size_t i;
+	size_t	count;
+	size_t	i;
 
 	i = 0;
 	count = 0;
-	while (s[i]) 
+	while (s[i] != '\0')
 	{
-		if (s[i] && s[i] != c)
+		if (s[i] != c)
 			count++;
 		while (s[i] && s[i] != c)
 			i++;
-		while (s[i] && s[i] == c) 
+		while (s[i] && s[i] == c)
 			i++;
 	}
-	printf("Step 1: how many substrings = %zu\n", count);
 	return (count);
 }
-static size_t str_len(const char *s, const char c) 
+
+static size_t	str_len(const char *s, const char c)
 {
-	size_t len;
+	size_t	len;
 
 	len = 0;
 	while (s[len] && s[len] != c)
 		len++;
-	printf("str_len = %zu\n", len);
 	return (len);
 }
-/*
-*/
-void ft_split(const char *s, const char c) 
+
+char	**ft_split(const char *s, const char c)
 {
 	char	**big;
 	size_t	little;
 	size_t	i;
 
+	if (!s)
+		return (NULL);
+	big = malloc(sizeof(char **) * (str_count(s, c) + 1));
 	little = 0;
-	printf("Step 0: pre malloc\n");
-	big = (char **)malloc(sizeof(char **) * (str_count(s, c) + 1));
-	free_all(big, little);
-	printf("Step 3: post free\n");
 	i = 0;
-	while (little < str_count(s, c)) 
+	while (s[i])
 	{
-		if (s[i] && s[i] != c)
+		if (s[i] != c)
 		{
-			big[little] = ft_substr(s, i, str_len((s + i), c) + 1); //free!
-			//free_all(big, little);
+			big[little] = ft_substr(s, i, str_len((s + i), c));
+			if (free_all(big, little))
+				return (NULL);
 			little++;
 		}
 		while (s[i] && s[i] != c)
@@ -78,17 +85,29 @@ void ft_split(const char *s, const char c)
 		while (s[i] && s[i] == c)
 			i++;
 	}
-	free_all(big, little);
-	/*
+	little++;
+	big[little] = NULL;
 	return (big);
-	*/
 }
-
+/*
 int	main(void)
 {
-	char *a = "1 22 333";
+	//char *a = "";
+	//const char c = '\0';
+	char *a = "hello !   2 3     5";
 	const char c = ' ';
-	
-	ft_split(a, c);
+	size_t	i = 0;
+
+	char **result = ft_split(a, 32);
+	if (!result)
+		free(result);
+	else
+	{
+		while (i <= str_count(a, 32))
+		{
+			printf("substring %zu == %s\n", i, result[i]);
+			i++;
+		}
+	}
 	return (0);
-}
+}*/
