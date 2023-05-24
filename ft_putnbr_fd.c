2421a897a1f1ad2a636a0af53b558923a2d7151c
Nicolas Maturana Chandia <nmaturan@car5s3.42barcelona.com>
2023-05-24 19:41:40 +0200

    Update: functional

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n / 10)
+		ft_putnbr_fd((n / 10) , fd);
+	ft_putchar_fd(n % 10 + '0', fd);
+}
+
+int	main(void)
+{
+	int	n = 0;
+	ft_putnbr_fd(n, 1);
+}
